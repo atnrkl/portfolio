5f5ee0c7118b13217d0755ad5e94e1b2d06ef547
atnrkl <ahmettanrikulu24@gmail.com>
2022-06-23 14:36:53 +0300

    portfolio

--- src/components/OnePager.jsx
+++ src/components/OnePager.jsx
@@ -8,7 +8,8 @@ const OnePager = () => {
         {/* photo */}
         <img
           className="photo"
-          src="https://pbs.twimg.com/profile_images/1419581357211197442/0sVdk2fh_400x400.jpg"
+          src={require("../images/pp.jpeg")}
+          /* src="https://pbs.twimg.com/profile_images/1419581357211197442/0sVdk2fh_400x400.jpg" */
           alt=""
         />
         {/* name */}
@@ -19,67 +20,160 @@ const OnePager = () => {
 
         {/* links */}
         <div className="links">
-          <h1 className="link">LinkedIn</h1>
-          <h1 className="link">Phone</h1>
-          <h1 className="link">CV</h1>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://tr.linkedin.com/in/atnrkl-cse?original_referer=https%3A%2F%2Fwww.google.com%2F"
+          >
+            <h1 className="link">LinkedIn</h1>
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={require("../files/AhmetTanrikuluResume.pdf")}
+          >
+            <h1 className="link">CV</h1>
+          </a>
         </div>
       </div>
 
       {/* grid section */}
       <div className="grid-section">
         {/* grid cards */}
-        <div className="grid-card">
-          <img
-            className="card-image"
-            src={require("../images/fitness.png")}
-            alt=""
-          />
-          <p className="title">NFT website</p>
-        </div>
+        <a
+          href="https://oxygenrepublic-sport-nextjs.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="grid-card">
+            <img
+              className="card-image"
+              src={require("../images/fitness.png")}
+              alt=""
+            />
+            <p className="title">GYM Website</p>
+          </div>
+        </a>
 
-        <div className="grid-card">
-          <img
-            className="card-image"
-            src={require("../images/maze.png")}
-            alt=""
-          />
-          <p className="title">NFT website</p>
-        </div>
+        <a
+          href="https://the-maze-nine.vercel.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="grid-card">
+            <img
+              className="card-image"
+              src={require("../images/maze.png")}
+              alt=""
+            />
+            <p className="title">Maze game (horror)</p>
+          </div>
+        </a>
 
-        <div className="grid-card">
-          <img
-            className="card-image"
-            src={require("../images/musicplayer.png")}
-            alt=""
-          />
-          <p className="title">NFT website</p>
-        </div>
+        <a
+          href="https://musicplayer-kappa.vercel.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="grid-card">
+            <img
+              className="card-image"
+              src={require("../images/musicplayer.png")}
+              alt=""
+            />
+            <p className="title">Web Music Player</p>
+          </div>
+        </a>
 
-        <div className="grid-card">
-          <img
-            className="card-image"
-            src={require("../images/nft.png")}
-            alt=""
-          />
-          <p className="title">NFT website</p>
-        </div>
+        <a
+          href="https://www.nightandgale.club/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="grid-card">
+            <img
+              className="card-image"
+              src={require("../images/nft.png")}
+              alt=""
+            />
+            <p className="title">NFT project website</p>
+          </div>
+        </a>
 
-        <div className="grid-card">
-          <img
-            className="card-image"
-            src={require("../images/parralax.png")}
-            alt=""
-          />
-          <p className="title">NFT website</p>
-        </div>
-        <div className="grid-card">
-          <img
-            className="card-image"
-            src={require("../images/realestate.png")}
-            alt=""
-          />
-          <p className="title">NFT website</p>
-        </div>
+        <a
+          href="https://parralax-scroll-react-app.vercel.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="grid-card">
+            <img
+              className="card-image"
+              src={require("../images/parralax.png")}
+              alt=""
+            />
+            <p className="title">Parralax website</p>
+          </div>
+        </a>
+
+        <a
+          href="https://real-estate-react-app.vercel.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="grid-card">
+            <img
+              className="card-image"
+              src={require("../images/realestate.png")}
+              alt=""
+            />
+            <p className="title">Luxury real estate website</p>
+          </div>
+        </a>
+
+        <a
+          href="https://www.crofunkylions.club/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="grid-card">
+            <img
+              className="card-image"
+              src={require("../images/funkylions.png")}
+              alt=""
+            />
+            <p className="title">NFT project website</p>
+          </div>
+        </a>
+
+        <a
+          href="https://www.lunafightclub.com/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="grid-card">
+            <img
+              className="card-image"
+              src={require("../images/lunafc.png")}
+              alt=""
+            />
+            <p className="title">NFT project website</p>
+          </div>
+        </a>
+
+        <a
+          href="https://www.secretcoinflip.com/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="grid-card">
+            <img
+              className="card-image"
+              src={require("../images/scrtcoinflip.png")}
+              alt=""
+            />
+            <p className="title">NFT project website</p>
+          </div>
+        </a>
       </div>
     </div>
   );
