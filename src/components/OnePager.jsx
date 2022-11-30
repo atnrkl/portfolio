8f1649c901a179fc06c2104536429a2336a3c2a7
atnrkl <ahmettanrikulu24@gmail.com>
2022-11-30 09:15:12 +0300

    github-link

--- src/components/OnePager.jsx
+++ src/components/OnePager.jsx
@@ -27,12 +27,15 @@ const OnePager = () => {
           >
             <h1 className="link">LinkedIn</h1>
           </a>
+          <a target="_blank" rel="noreferrer" href="https://github.com/atnrkl">
+            <h1 className="link">CV</h1>
+          </a>
           <a
             target="_blank"
             rel="noreferrer"
             href={require("../files/AhmetTanrikuluResume.pdf")}
           >
-            <h1 className="link">CV</h1>
+            <h1 className="link">Github</h1>
           </a>
         </div>
       </div>
